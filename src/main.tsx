5eab1aa2b37c7f7c005e3128cb13ea536ff636be
Advant0-st4r <advantoincorporated@gmail.com>
2025-10-03 00:54:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { ClerkProvider } from '@clerk/clerk-react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY // Add in Phase 1 from env
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY}> */}
+        <App />
+      {/* </ClerkProvider> */}
+    </Router>
+  </React.StrictMode>,
+)
