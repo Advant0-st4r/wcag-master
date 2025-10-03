49212d10f0ee2a555984306c8d3e938bcb4b9e8e
Advant0-st4r <advantoincorporated@gmail.com>
2025-10-03 03:15:48 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,18 +1,6 @@
-// src/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-// Make sure you add these to your .env file at the root of the project
-// VITE_SUPABASE_URL=your_supabase_project_url
-// VITE_SUPABASE_ANON_KEY=your_supabase_anon_key
+const SUPABASE_URL = process.env.SUPABASE_URL!
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    'Supabase URL or anon key not found. Please check your .env file.'
-  )
-}
-
-// Export a single Supabase client instance
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
